b9b8bfdf4a2bd643bca3abf65cce31a6084b302d
Egor Pushkin <egor@glympse.com>
2015-08-04 10:03:17 -0700

    Image tagging was updated.

--- build.sh
+++ build.sh
@@ -7,12 +7,14 @@ IMAGE_NAME=tools/sync-agent
 # Helpers
 IMAGE_TAG=$(git rev-parse --short HEAD)
 TAGGED_IMAGE=$REGISTRY_URL/$IMAGE_NAME:$IMAGE_TAG
+LATEST_IMAGE=$REGISTRY_URL/$IMAGE_NAME:latest
 
 # Build the image
 docker build -t $IMAGE_NAME .
 
 # Mark image with registry URL and tag
 docker tag -f $IMAGE_NAME $TAGGED_IMAGE
+docker tag -f $IMAGE_NAME $LATEST_IMAGE
 
 # Push image to repote registry
-docker push $TAGGED_IMAGE
+docker push $LATEST_IMAGE
